d70b8b97de6efe5e59fe8f85082d9fb5e4b0f27d
eugene9212 <eugene12@korea.ac.kr>
2018-11-20 11:56:08 +0900

    Practice the sample data in Package

--- fda.usc_package.R
+++ fda.usc_package.R
@@ -1,38 +1,71 @@
 # Practice on functional data example #
+rm(list = ls())
 
 # load packages
 library(fda); library(fda.usc)
+
 # load data
 data(tecator)
 x=tecator$absorp.fdata
-str(x)
-y=tecator$y$Fat
-str(y)
+y.Fat=tecator$y$Fat
 
+# -------------------------------- For Gaussian ---------------------------------------- #
 # data trimming(1)
 tt=x[["argvals"]] # extract time range from x(fdata)
 dataf=as.data.frame(tecator$y) # transform y as data.frame structure
 nbasis.x=11 # create basis used for fdata or fd covariates.
-nbasis.b=7  # create basis used for beta parameter estimation.
+nbasis.b=4  # create basis used for beta parameter estimation.
+
+# formula
+f=Fat~Protein+x 
 
 # create basis
 basis1=create.bspline.basis(rangeval=range(tt),nbasis=nbasis.x)
 basis2=create.bspline.basis(rangeval=range(tt),nbasis=nbasis.b)
 
-# formula
+# Fitting (train) _ gaussian
+basis.x=list("x"=basis1) # has to be the same name
+basis.b=list("x"=basis2)
+ldata=list("df"=dataf,"x"=x)
+res=fregre.glm(f,family=gaussian(), data=ldata, basis.x=basis.x, basis.b=basis.b)
+summary(res)
+fres$basis.b
+res$beta.l
+?fregre.glm
+
+# summary
+summary(res)
+
+# -------------------------------- For Binomial ---------------------------------------- #
+# data trimming(1)
+tt=x[["argvals"]] # extract time range from x(fdata)
+
+## Change y into 1 or 0
+y1 <- as.vector(y.Fat)
+y1[which(y1 < 13)] = 0
+y1[which(y1 >= 13)] = 1
+
+## formula
 f=Fat~Protein+x 
 # y : Fat, Water, Protein
 # x : fdata of tecator$absorp
 
-# data trimming(2) : basis
-basis.x=list("x"=basis1)
+# Create basis
+nbasis.x=11 # create basis used for fdata or fd covariates.
+nbasis.b=10  # create basis used for beta parameter estimation.
+
+basis1=create.bspline.basis(rangeval=range(tt),nbasis=nbasis.x)
+basis2=create.bspline.basis(rangeval=range(tt),nbasis=nbasis.b)
+
+# Fitting (train) _ gaussian
+basis.x=list("x"=basis1) # has to be the same name
 basis.b=list("x"=basis2)
-ldata=list("df"=dataf,"x"=x) 
-# dataf : no fdata
-# x : basis1 and basis2
 
-# Fitting (train)
-res=fregre.glm(f,family=gaussian(),data=ldata,basis.x=basis.x,basis.b=basis.b)
+# Appropriate data form
+dataf=as.data.frame(tecator$y)
+dataf$Fat = y1
+ldata=list("df"=dataf,"x"=x)
 
-# summary
+# Fitting (train) _ binomial
+res=fregre.glm(f,binomial(link = "logit"),data=ldata, basis.x=basis.x, basis.b=basis.b)
 summary(res)
