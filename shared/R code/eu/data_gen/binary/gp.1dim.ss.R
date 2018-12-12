f8ba3c2b6d15c3316dfec411f735d73e17f0a2b7
eugene9212 <eugene12@korea.ac.kr>
2018-12-12 14:21:36 +0900

    Separate data_gen folder for binary classification

---
+++ shared/R code/eu/data_gen/binary/gp.1dim.ss.R
@@ -0,0 +1,49 @@
+# create Gaussian Process (1-dimension)
+gp.1dim.ss <- function(n, error, beta, t = seq(0, 1, by = 0.05), seed = 1)
+{
+  set.seed(seed)
+  # binary response
+  y <- rep(1, n)
+  y[1:(n/2)] <- -1
+  
+  # spatio temporal predictor
+  n.t <- length(t) 
+  
+  # covariance matrix of gaussian process
+  Sigma <- error*diag(n.t)
+  
+  # Divide index
+  idx1 <- which(y==-1)
+  idx2 <- which(y==1)
+  
+  # Create mean vector
+  mu.t1 <- sin(t)
+  mu.t2 <- sin(t) + beta
+  
+  # x.list
+  x.list <- as.list(1:n)
+  for (i in idx1) {
+    z.t <- rmvnorm(1, mu.t1, Sigma)
+    x.list[[i]] <- z.t
+  }
+  
+  for (i in idx2) {
+    z.t <- rmvnorm(1, mu.t2, Sigma)
+    x.list[[i]] <- z.t
+  }
+  
+  # Calculate the True p
+  true.p <- rep(0,n)
+  for(i in 1:n){
+    a <- dmvnorm(x=x.list[[i]], mean = mu.t1,log=TRUE)
+    b <- dmvnorm(x=x.list[[i]], mean = mu.t2,log=TRUE)
+    total <- exp(a)+exp(b)
+    if (i %in% idx1) true.p[i] <- exp(a)/total
+    else if (i %in% idx2) true.p[i] <- exp(b)/total
+    else warning(paste0("No class was assigned for obs ",i))
+  }
+  
+  obj <- list(x = x.list, y = y, t = t, true.p = true.p)
+  return(obj)
+}
+
